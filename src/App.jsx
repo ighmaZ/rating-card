cf9709f24dc23026512fef3cb27623ddecceb262
ighmaz <ighmazcool@gmail.com>
2022-09-16 22:46:05 +0530

    initialized

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+import Card from './component/Card'
+
+const App = () => {
+  return (
+    <div className='container'>
+     <Card/>
+    </div>
+  )
+}
+
+export default App
