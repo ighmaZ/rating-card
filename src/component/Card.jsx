74fd250932df522a5f7dfa5a1b67d98521e8f0e1
ighmaz <ighmazcool@gmail.com>
2022-09-17 12:51:40 +0530

    added first page

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,22 +1,11 @@
 
 import React from 'react'
-import {AiFillStar} from 'react-icons/ai'
+import FirstPage from './FirstPage'
 const Card = () => {
   return (
     <div className='card'>
 
-<div className='fill'><AiFillStar/></div>
-<h2>How did we do?</h2>
-<p>Please let us know how we did with your support request. all feedback is appreciated to help us improve our offering!</p>
-<div className='button-container'>
-
-<button className='button'>1</button>
-<button className='button'>2</button>
-<button className='button'>3</button>
-<button className='button'>4</button>
-<button className='button'>5</button>
-</div>
-<button className='submit'>SUBMIT</button>
+<FirstPage/>
 
     </div>
   )
---
+++ src/component/FirstPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {AiFillStar} from 'react-icons/ai'
+
+const FirstPage = () => {
+  return (
+    <>
+<div className='fill'><AiFillStar/></div>
+<h2>How did we do?</h2>
+<p>Please let us know how we did with your support request. all feedback is appreciated to help us improve our offering!</p>
+<div className='button-container'>
+
+<button className='button'>1</button>
+<button className='button'>2</button>
+<button className='button'>3</button>
+<button className='button'>4</button>
+<button className='button'>5</button>
+</div>
+<button className='submit'>SUBMIT</button>
+
+    </>
+  )
+}
+
+export default FirstPage
